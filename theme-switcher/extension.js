04a6463558b078a8fa6bc0e0a16c27ef8f35d7ad
Maksym Zavalniuk <mezgoodle@gmail.com>
2024-12-02 10:49:02 +0200

    Write new command

--- theme-switcher/extension.js
+++ theme-switcher/extension.js
@@ -1,36 +1,125 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const vscode = require("vscode");
 
 /**
  * @param {vscode.ExtensionContext} context
  */
-function activate(context) {
+async function activate(context) {
+  console.log("Theme Switcher activated!");
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "theme-switcher" is now active!');
+  const config = vscode.workspace.getConfiguration("themeSwitcher");
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('theme-switcher.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+  async function getTheme(type) {
+    const availableThemes = vscode.extensions.all
+      .flatMap((extension) => extension.packageJSON.contributes?.themes || [])
+      .map((theme) => theme.label || theme.id);
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Theme Switcher!');
-	});
+    return await vscode.window.showQuickPick(availableThemes, {
+      placeHolder: `Select ${type} theme`,
+    });
+  }
 
-	context.subscriptions.push(disposable);
+  async function getHour(type) {
+    let input = await vscode.window.showInputBox({
+      prompt: `Enter ${type} hour (0-23)`,
+      validateInput: (value) => {
+        const num = parseInt(value);
+        if (isNaN(num) || num < 0 || num > 23) {
+          return "Please enter a number between 0 and 23";
+        }
+        return null;
+      },
+    });
+
+    return input !== undefined ? parseInt(input) : undefined;
+  }
+
+  async function promptForSettings() {
+    const lightTheme = await getTheme("light");
+    if (lightTheme) {
+      await config.update(
+        "lightTheme",
+        lightTheme,
+        vscode.ConfigurationTarget.Global
+      );
+    }
+
+    const darkTheme = await getTheme("dark");
+    if (darkTheme) {
+      await config.update(
+        "darkTheme",
+        darkTheme,
+        vscode.ConfigurationTarget.Global
+      );
+    }
+
+    const startTime = await getHour("start");
+    if (startTime !== undefined) {
+      await config.update(
+        "startTime",
+        startTime,
+        vscode.ConfigurationTarget.Global
+      );
+    }
+
+    const endTime = await getHour("end");
+    if (endTime !== undefined) {
+      await config.update(
+        "endTime",
+        endTime,
+        vscode.ConfigurationTarget.Global
+      );
+    }
+  }
+
+  function setTheme(themeName) {
+    if (themeName) {
+      vscode.workspace
+        .getConfiguration("workbench")
+        .update("colorTheme", themeName, vscode.ConfigurationTarget.Global);
+    }
+  }
+
+  async function checkTimeAndSwitchTheme() {
+    const lightThemeSetting = config.get("lightTheme");
+    const darkThemeSetting = config.get("darkTheme");
+    const startTimeSetting = config.get("startTime");
+    const endTimeSetting = config.get("endTime");
+
+    if (
+      lightThemeSetting &&
+      darkThemeSetting &&
+      startTimeSetting !== undefined &&
+      endTimeSetting !== undefined
+    ) {
+      const now = new Date();
+      const hour = now.getHours();
+
+      if (hour >= startTimeSetting && hour < endTimeSetting) {
+        setTheme(lightThemeSetting);
+      } else {
+        setTheme(darkThemeSetting);
+      }
+    } else {
+      await promptForSettings();
+      // Викликаємо рекурсивно, щоб перевірити час та встановити тему після отримання налаштувань
+      await checkTimeAndSwitchTheme();
+    }
+  }
+
+  await checkTimeAndSwitchTheme(); // Чекаємо завершення першої перевірки
+  setInterval(checkTimeAndSwitchTheme, 60000);
+
+  let disposable = vscode.commands.registerCommand(
+    "theme-switcher.switchTheme",
+    checkTimeAndSwitchTheme
+  );
+
+  context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate,
+};
